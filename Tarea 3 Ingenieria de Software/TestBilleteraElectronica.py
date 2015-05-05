d64ade463ab52e0f56ed0c27fa2bc13a1c9c22ce
edwin <e.murillo0408@gmail.com>
2015-05-05 16:08:43 -0430

    testSaldoRecargaNegativa

--- Tarea 3 Ingenieria de Software/TestBilleteraElectronica.py
+++ Tarea 3 Ingenieria de Software/TestBilleteraElectronica.py
@@ -45,3 +45,7 @@ class Test(unittest.TestCase):
         Arleyn = BilleteraElectronica.BilleteraElectronica("001","Arleyn","Goncalves","21467704")
         Arleyn.recargar(100,'27/12/1992','01')
         self.assertRaises(Exception, lambda: Arleyn.consumir(150,'28/12/1992','02'))
+        
+    def testSaldoRecargaNegativa(self):
+        Arleyn = BilleteraElectronica.BilleteraElectronica("001","Arleyn","Goncalves","21467704")
+        self.assertRaises(Exception, lambda: Arleyn.recargar(-150,'28/12/1992','02'))
