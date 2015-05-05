2956a586867708c853119dbda21f9517dc494885
user.Arleyn <arleyn.g.2712@hotmail.com>
2015-05-05 14:44:19 -0430

    Segundo Commit (Segundo Test)

--- Tarea 3 Ingenieria de Software/TestBilleteraElectronica.py
+++ Tarea 3 Ingenieria de Software/TestBilleteraElectronica.py
@@ -7,3 +7,8 @@ class Test(unittest.TestCase):
 
     def testBilleteraExist(self):
         pass
+    
+    def testCrearBilletera(self):
+        Arleyn = BilleteraElectronica.BilleteraElectronica("001","Arleyn","Goncalves","21467704");
+        saldo = Arleyn.saldo();
+        self.assertEqual(0,saldo);
