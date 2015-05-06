cda19b0806ed2c4f7673b7f1059f3eeea8575ec5
edwin <e.murillo0408@gmail.com>
2015-05-05 20:15:38 -0430

    Refactorizacion testListaConsumo

--- Tarea 3 Ingenieria de Software/BilleteraElectronica.py
+++ Tarea 3 Ingenieria de Software/BilleteraElectronica.py
@@ -5,6 +5,14 @@ Created on 3/5/2015
 '''
 #-*- coding:UTF-8 -*-
 
+class DatosDebitos(object):
+    def __init__(self):
+        self._listas_consumos = [];
+    
+    def Almacenar(self,cantidad,fecha,identificar):
+        self._listas_consumos.append((cantidad,fecha,identificar))
+        return self._listas_consumos
+
 class BilleteraElectronica(object):
     def __init__(self,identificador,nombre,apellido,CI):
         self._identificador = identificador;
@@ -12,6 +20,7 @@ class BilleteraElectronica(object):
         self._apellido = apellido
         self._CI = CI;
         self._saldo = 0;
+        self._Consumos = DatosDebitos();
         
     def saldo(self):
         print(self._saldo);
@@ -31,4 +40,5 @@ class BilleteraElectronica(object):
             raise Exception("La cantidad a consumir tiene que ser mayor a 0");
         else:
             self._saldo -= cantidad;
+            self._Consumos.Almacenar(cantidad,fecha,id_estacionamiento);
             return self._saldo;
